75d366a1eabdd6b469ee4f9c8eb59953415ac2a3
daydaddy <jn_day@hotmail.com>
2020-10-01 14:59:39 -0600

    added Leap exercism

---
+++ leap.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isLeap = (year) => {
+
+  let testLeap = false
+
+  if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+    testLeap = true
+  }
+
+  return testLeap
+};
